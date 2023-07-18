43d7c125e33c3dfb1465e1b1a71fdd1ded7cecd2
Anshukumar-Digit <139847620+Anshukumar-Digit@users.noreply.github.com>
2023-07-18 12:41:20 +0530

    Update TestingCode.java

--- src/main/java/com/digitTestproj/p1/TestingCode.java
+++ src/main/java/com/digitTestproj/p1/TestingCode.java
@@ -4,7 +4,9 @@ public class TestingCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello");
+System.out.println("Hello anshu");
+System.out.println("Hello rahul");
+		
 	}
 
 }
